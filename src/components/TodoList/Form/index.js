9c17ddd5779ea6c4c22df995afa6f9b5a3712468
Nick Ray <nickrray@gmail.com>
2020-08-07 14:47:13 -0500

    :wheelchair: :lipstick: Keep labels present but hide from normal UI

--- src/components/TodoList/Form/index.js
+++ src/components/TodoList/Form/index.js
@@ -5,7 +5,7 @@ export const Form = ({ handler }) => {
 
   return (
     <form onSubmit={handler}>
-      <label htmlFor="new-todo">New Todo</label>
+      <label className="screen-reader-text" htmlFor="new-todo">New Todo</label>
       <input id="new-todo" placeholder="New Todo..." type="text" />
       <button type="submit">Add Todo</button>
     </form>
