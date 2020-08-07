34c5f7c6f84fae730fba9e874fc1813f77889682
Nick Ray <nickrray@gmail.com>
2020-08-07 13:20:10 -0500

    :construction: Grab and use passed submit handler

--- src/components/TodoList/Form/index.js
+++ src/components/TodoList/Form/index.js
@@ -1,9 +1,9 @@
 import React from 'react'
 
-export const Form = () => {
+export const Form = ({ handler }) => {
 
   return (
-    <form>
+    <form onSubmit={handler}>
       <label htmlFor="new-todo">New Todo</label>
       <input id="new-todo" placeholder="New Todo..." type="text" />
       <button type="submit">Add Todo</button>
