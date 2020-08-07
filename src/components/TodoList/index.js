8926a4b86ab63381458546c48bc26bcfc3bf3c06
Nick Ray <nickrray@gmail.com>
2020-08-07 13:19:23 -0500

    :construction: Setup piece of state for todos

--- src/components/TodoList/index.js
+++ src/components/TodoList/index.js
@@ -1,9 +1,10 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import { Form } from './Form'
 import { List } from './List'
 
 export const TodoList = () => {
+  const [todos, setTodos] = useState([])
 
   return (
     <div>
