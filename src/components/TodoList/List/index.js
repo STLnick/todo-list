3d3ee17d2d53c5443df7ddbcc7c724828fed46bd
Nick Ray <nickrray@gmail.com>
2020-08-07 13:25:05 -0500

    :construction: Setup fxn to render todos

--- src/components/TodoList/List/index.js
+++ src/components/TodoList/List/index.js
@@ -1,8 +1,22 @@
 import React from 'react'
 
-export const List = () => {
+export const List = ({ currentTodos }) => {
+
+  const renderTodos = () => {
+    return currentTodos.map((todo, i) => {
+      return (
+        <li key={i}>
+          {todo}
+          <label htmlFor="completed" >Completed</label>
+          <input type="checkbox" id="completed" />
+        </li>
+      )
+    })
+  }
 
   return (
-    <p>List</p>
+    <ol>
+      {renderTodos()}
+    </ol>
   )
 }
