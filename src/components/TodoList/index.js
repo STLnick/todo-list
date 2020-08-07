24f4df88d427c8e0b755926803d27106d46c4fb6
Nick Ray <nickrray@gmail.com>
2020-08-07 13:12:14 -0500

    :seedling: Starter files with imports and exports setup

---
+++ src/components/TodoList/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import { Form } from './Form'
+import { List } from './List'
+
+export const TodoList = () => {
+
+  return (
+    <div>
+      <h2>Todo List</h2>
+      <List />
+      <Form />
+    </div>
+  )
+}
