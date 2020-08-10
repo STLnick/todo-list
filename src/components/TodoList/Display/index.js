ec0ee8696b2ff0580d753f50e91d499afa683cde
Nick Ray <nickrray@gmail.com>
2020-08-10 13:55:37 -0500

    :art: Change to show yellow bg on more than half of todos completed

--- src/components/TodoList/Display/index.js
+++ src/components/TodoList/Display/index.js
@@ -4,17 +4,19 @@ import PropTypes from 'prop-types'
 export const Display = ({ numComplete, numTodos }) => {
 
   const getBackgroundAndTextClass = () => {
-    if (numComplete === 0) {
+    if (numComplete < numTodos / 2) {
       return 'has-background-danger has-text-white'
-    } else if (numComplete < numTodos) {
-      return 'has-background-warning has-text-grey'
+    } else if (numComplete === numTodos) {
+      return 'has-background-success has-text-white'
     }
-    return 'has-background-success has-text-white'
+
+    return 'has-background-warning has-text-grey'
   }
 
   return (
-    <div>
-      <p className={`has-text-centered mb-4 py-2 has-text-weight-bold ${getBackgroundAndTextClass()}`}>
+    <div className="has-text-centered has-text-weight-semibold">
+      <p>{new Date().toDateString()}</p>
+      <p className={`mb-4 py-2 has-text-weight-bold ${getBackgroundAndTextClass()}`}>
         <span className="completed-todos">
           {`${numComplete} / ${numTodos} `}
         </span>
