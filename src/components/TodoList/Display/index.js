f959632a1417068f92ae3921c353f1c57a337cc7
Nick Ray <nickrray@gmail.com>
2020-08-10 13:52:00 -0500

    :sparkles: Change bg color depending on numCompleted

--- src/components/TodoList/Display/index.js
+++ src/components/TodoList/Display/index.js
@@ -3,9 +3,18 @@ import PropTypes from 'prop-types'
 
 export const Display = ({ numComplete, numTodos }) => {
 
+  const getBackgroundAndTextClass = () => {
+    if (numComplete === 0) {
+      return 'has-background-danger has-text-white'
+    } else if (numComplete < numTodos) {
+      return 'has-background-warning has-text-grey'
+    }
+    return 'has-background-success has-text-white'
+  }
+
   return (
     <div>
-      <p className="has-text-centered mb-4 py-2 has-text-weight-bold has-background-success has-text-white">
+      <p className={`has-text-centered mb-4 py-2 has-text-weight-bold ${getBackgroundAndTextClass()}`}>
         <span className="completed-todos">
           {`${numComplete} / ${numTodos} `}
         </span>
