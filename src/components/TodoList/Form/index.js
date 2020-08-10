1b1152a9d9037470446bfa61fdf2deb0261aef3b
Nick Ray <nickrray@gmail.com>
2020-08-10 14:04:37 -0500

    :lipstick: Establish better focus/hover styling for submit button

--- src/components/TodoList/Form/index.js
+++ src/components/TodoList/Form/index.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
+import './Form.css'
+
 export const Form = ({ handler }) => {
 
   return (
@@ -9,7 +11,13 @@ export const Form = ({ handler }) => {
         <label className="screen-reader-text" htmlFor="new-todo">New Todo</label>
         <input className="input is-rounded is-success" id="new-todo" placeholder="New Todo..." type="text" />
         <p className="help">e.g. Take out the Trash</p>
-        <button className="button is-success is-uppercase has-text-weight-bold" type="submit">Add Todo</button>
+        <button
+          className="button is-success is-uppercase has-text-weight-bold"
+          id="submitBtn"
+          type="submit"
+        >
+          Add Todo
+        </button>
       </div>
     </form>
   )
