b40bb254eb78050882746d6ceab5be3a9558a611
Nick Ray <nickrray@gmail.com>
2020-08-10 14:04:54 -0500

    :lipstick: Improve contrast when background is yellow

--- src/components/TodoList/Display/index.js
+++ src/components/TodoList/Display/index.js
@@ -9,12 +9,11 @@ export const Display = ({ numComplete, numTodos }) => {
     } else if (numComplete === numTodos) {
       return 'has-background-success has-text-white'
     }
-
     return 'has-background-warning has-text-grey'
   }
 
   return (
-    <div className="has-text-centered has-text-weight-semibold">
+    <div className="has-text-centered has-text-weight-semibold has-text-grey">
       <p>{new Date().toDateString()}</p>
       <p className={`mb-4 py-2 has-text-weight-bold ${getBackgroundAndTextClass()}`}>
         <span className="completed-todos">
