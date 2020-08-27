0af9a73b3563f6165ef1c16a5c78bad15f00efa5
Nick Ray <nickrray@gmail.com>
2020-08-27 11:24:06 -0500

    :construction: Start a basic form with formik

--- src/views/Login.js
+++ src/views/Login.js
@@ -1,5 +1,45 @@
 import React from 'react'
 
+import { useFormik } from 'formik'
+
 export const Login = () => {
-  return <h2>Login Page</h2>
+  const formik = useFormik({
+    initialValues: {
+      email: '',
+      password: '',
+      name: ''
+    },
+    onSubmit: values => {
+      console.log('submission: ', values)
+    }
+  })
+
+
+  return (
+    <form onSubmit={formik.handleSubmit}>
+      <h2>Login Page</h2>
+      <input
+        type="text"
+        name="name"
+        id="name"
+        onChange={formik.handleChange}
+        placeholder="Name"
+        value={formik.values.name} />
+      <input
+        type="email"
+        name="email"
+        id="email"
+        onChange={formik.handleChange}
+        placeholder="Email"
+        value={formik.values.email} />
+      <input
+        type="password"
+        name="password"
+        id="password"
+        onChange={formik.handleChange}
+        placeholder="Password"
+        value={formik.values.password} />
+      <button type="submit">Login</button>
+    </form>
+  )
 }
