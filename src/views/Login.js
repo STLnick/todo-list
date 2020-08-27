080e575660762d886a7c91d2fc359e64f7295202
Nick Ray <nickrray@gmail.com>
2020-08-27 12:33:06 -0500

    :recycle: Refactored again to simplify use of Formik with HOCs
    Higher Order Components

--- src/views/Login.js
+++ src/views/Login.js
@@ -1,56 +1,38 @@
 import React from 'react'
-import { useFormik } from 'formik'
+import { ErrorMessage, Field, Form, Formik } from 'formik'
 import * as Yup from 'yup'
 
-export const Login = () => {
+export const Login = () =>
+  <div className="box has-text-centered mx-3">
+    <Formik
+      initialValues={{
+        email: '',
+        password: '',
+        name: ''
+      }}
+      validationSchema={Yup.object({
+        name: Yup.string().required('Name is required'),
+        email: Yup.string().email('Invalid email address!').required('Email is required'),
+        password: Yup.string().min(6).required('Password is required'),
+      })}
+      onSubmit={(values, { setSubmitting }) => {
+        console.log('submission: ', values)
+        setSubmitting(false)
+      }}
+    >
+      <Form>
+        <label htmlFor="name">Name</label>
+        <Field name="name" type="text" />
+        <ErrorMessage name="name" />
 
-  const formik = useFormik({
-    initialValues: {
-      email: '',
-      password: '',
-      name: ''
-    },
-    validationSchema: Yup.object({
-      name: Yup.string().required('Name is required'),
-      email: Yup.string().email('Invalid email address!').required('Email is required'),
-      password: Yup.string().min(6).required('Password is required'),
-    }),
-    onSubmit: values => {
-      console.log('submission: ', values)
-    }
-  })
+        <label htmlFor="email">Email</label>
+        <Field name="email" type="text" />
+        <ErrorMessage name="email" />
 
-  return (
-    <form onSubmit={formik.handleSubmit}>
-      <h2>Login Page</h2>
-      <input
-        type="text"
-        name="name"
-        id="name"
-        onBlur={formik.handleBlur}
-        onChange={formik.handleChange}
-        placeholder="Name"
-        value={formik.values.name} />
-      {formik.touched.name && formik.errors.name ? <p className="has-text-danger">{formik.errors.name}</p> : null}
-      <input
-        type="text"
-        name="email"
-        id="email"
-        onBlur={formik.handleBlur}
-        onChange={formik.handleChange}
-        placeholder="Email"
-        value={formik.values.email} />
-      {formik.touched.email && formik.errors.email ? <p className="has-text-danger">{formik.errors.email}</p> : null}
-      <input
-        type="password"
-        name="password"
-        id="password"
-        onBlur={formik.handleBlur}
-        onChange={formik.handleChange}
-        placeholder="Password"
-        value={formik.values.password} />
-      {formik.touched.password && formik.errors.password ? <p className="has-text-danger">{formik.errors.password}</p> : null}
-      <button type="submit">Login</button>
-    </form>
-  )
-}
+        <label htmlFor="password">Password</label>
+        <Field name="password" type="password" />
+        <ErrorMessage name="password" />
+        <button className="button is-primary" type="submit">Login</button>
+      </Form>
+    </Formik>
+  </div>
