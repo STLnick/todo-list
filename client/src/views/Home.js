69799ff14392de3c7137a02e40b29df92a97dae4
Nick Ray <nickrray@gmail.com>
2020-09-01 13:59:53 -0500

    :fire: Remove extra newlines

--- client/src/views/Home.js
+++ client/src/views/Home.js
@@ -2,8 +2,6 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 export const Home = () => {
-
-
   return <div>
     <section className="hero is-fullheight">
       <div className="hero-body">
