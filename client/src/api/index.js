001f3956ff0debbed49715691170cd5a3ea070b6
Nick Ray <nickrray@gmail.com>
2020-09-02 14:47:01 -0500

    :recycle: Change api into function factory with a default baseUrl parameter

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,6 @@
-export default {
+export default (baseUrl = 'http://localhost:5000') => ({
   async index() {
     const res = await fetch('https://my-json-server.typicode.com/Claim-Academy-JS/todos/todos')
     return await res.json()
-  }
+  },
 }
